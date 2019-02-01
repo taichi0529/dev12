4b4f40021d2c29c258fb1e0d808e7b49a1fa9ba7
Taichi <taichi@asaichi.co.jp>
2019-02-01 23:37:06 +0900

    day4の状態

---
+++ todo/Controllers/TaskListTableViewController.swift
@@ -0,0 +1,84 @@
+//
+//  TaskListTableViewController.swift
+//  todo
+//
+//  Created by 中村太一 on 2018/09/22.
+//  Copyright © 2018年 中村太一. All rights reserved.
+//
+
+import UIKit
+
+class TaskListTableViewController: UITableViewController, TaskCollectionDelegate {
+    
+    let taskCollection = TaskCollection.shared
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        taskCollection.delegate = self
+        
+    }
+    
+    // デリゲート
+    func saved() {
+        print ("saved")
+        self.tableView.reloadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+    }
+    
+    @IBAction func didTouchAddButton(_ sender: Any) {
+        self.performSegue(withIdentifier: "showToAddViewController", sender: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return taskCollection.taskCount()
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        print ("Section: " + String(indexPath.section) + " row:" + String(indexPath.row))
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // Configure the cell...
+        
+        cell.textLabel?.text = taskCollection.getTask(at: indexPath.row).title
+
+        return cell
+    }
+
+    // セルの選択
+    override func tableView(_ table: UITableView,didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: true)
+        let taskViewController = storyboard?.instantiateViewController(withIdentifier: "TaskViewController") as! TaskViewController
+        taskViewController.selectedTask = taskCollection.getTask(at: indexPath.row)
+        self.navigationController?.pushViewController(taskViewController, animated: true)
+        //        performSegue(withIdentifier: "showToTaskViewController", sender: selectedTask)
+    }
+    
+    // スワイプで削除
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        taskCollection.removeTask(at: indexPath.row)
+    }
+
+}
---
+++ todo/Controllers/TaskViewController.swift
@@ -0,0 +1,88 @@
+//
+//  AddViewController.swift
+//  todo
+//
+//  Created by 中村太一 on 2018/09/22.
+//  Copyright © 2018年 中村太一. All rights reserved.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+    
+    let taskCollection = TaskCollection.shared
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var noteTextView: UITextView!
+    
+    var selectedTask: Task?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let selectedTask = self.selectedTask {
+            self.title = "編集"
+            self.titleTextField.text = selectedTask.title
+            self.noteTextView.text = selectedTask.note
+        }
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapGesture))
+        self.view.addGestureRecognizer(tapRecognizer)
+        
+        
+    }
+    @IBAction func didTouchSaveButton(_ sender: Any) {
+        guard let title = titleTextField.text else {
+            return
+        }
+        if (title.isEmpty) {
+            showAlert("タイトルを入力して下さい。")
+            return
+        }
+
+        if let selectedTask = self.selectedTask {
+            selectedTask.title = title
+            selectedTask.note = noteTextView.text
+            taskCollection.editTask()
+        } else {
+            let task = Task()
+            task.title = title
+            task.note = noteTextView.text
+            taskCollection.addTask(task)
+        }
+        
+        self.navigationController?.popViewController(animated: true)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func showAlert(_ text: String){
+        let alertController = UIAlertController(title: "エラー", message: text , preferredStyle: UIAlertController.Style.alert)
+        let action = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (action) -> Void in
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    //画面をタップすると、キーボードが閉じる動作
+    @objc func tapGesture(sender: UITapGestureRecognizer) {
+        titleTextField.resignFirstResponder()
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
